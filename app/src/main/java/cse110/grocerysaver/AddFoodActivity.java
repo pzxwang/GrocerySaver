c29ef4425e7d92a0245d81a9a343250fc6f62a3b
Philip Pesca <phil.pesca@gmail.com>
2017-02-27 02:22:02 -0800

    Add data validation

--- app/src/main/java/cse110/grocerysaver/AddFoodActivity.java
+++ app/src/main/java/cse110/grocerysaver/AddFoodActivity.java
@@ -10,6 +10,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -51,7 +52,11 @@ public class AddFoodActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case R.id.menu_add_food_done:
-                SimpleDateFormat format = new SimpleDateFormat("MM/dd/yy");
+                if (!isDataValid()) {
+                    return false;
+                }
+
+                SimpleDateFormat format = new SimpleDateFormat("MMM dd, yyyy");
                 Calendar expiration = Calendar.getInstance();
                 try {
                     expiration.setTime(format.parse(expDateFld.getText().toString()));
@@ -74,5 +79,29 @@ public class AddFoodActivity extends AppCompatActivity {
     public void showDatePickerDialog(View v) {
         DialogFragment newFragment = new DatePickerFragment();
         newFragment.show(getSupportFragmentManager(), "datePicker");
+    }
+
+    private boolean validateFoodName() {
+        return nameFld.getText().toString().isEmpty();
+    }
+
+    private  boolean validateExpirationDate() {
+        return expDateFld.getText().toString().isEmpty();
+    }
+
+    private boolean isDataValid() {
+        if (nameFld.getText().toString().isEmpty()) {
+            Toast.makeText(this, "Please enter food name.", Toast.LENGTH_SHORT).show();
+
+            return false;
+        }
+
+        if (expDateFld.getText().toString().isEmpty()) {
+            Toast.makeText(this, "Please enter an expiration date.", Toast.LENGTH_SHORT).show();
+
+            return false;
+        }
+
+        return true;
     }
 }
