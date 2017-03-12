9cf85bae1776d21b3cfaf92858e8ae2ac5bfe2f9
Philip Wang <zow006@ucsd.edu>
2017-03-11 16:25:13 -0800

    Begin parsing json file in assets to insert into inventory (recommended dates). NOT FUNCTIONAL YET.

--- app/src/main/java/cse110/grocerysaver/database/DatabaseHelper.java
+++ app/src/main/java/cse110/grocerysaver/database/DatabaseHelper.java
@@ -1,8 +1,15 @@
 package cse110.grocerysaver.database;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.text.format.DateUtils;
+import android.util.JsonReader;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 import cse110.grocerysaver.database.DatabaseContract.FridgeItem;
 import cse110.grocerysaver.database.DatabaseContract.FoodItem;
@@ -58,8 +65,13 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     private static final String SQL_DROP_TABLE_INVENTORY_ITEM =
             "DROP TABLE IF EXISTS " + InventoryItem.TABLE;
 
+    private static final String INV_FILENAME = "inventoryData.json";
+
+    private Context context;
+
     public DatabaseHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
+        this.context = context;
     }
 
     @Override
@@ -68,6 +80,113 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         db.execSQL(SQL_CREATE_TABLE_FAVORITE);
         db.execSQL(SQL_CREATE_TABLE_FOOD_ITEM);
         db.execSQL(SQL_CREATE_TABLE_INVENTORY_ITEM);
+        try {
+            InputStream in =  context.getAssets().open(INV_FILENAME);
+            readInventory (db, in);
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void readInventory(SQLiteDatabase db, InputStream in) throws IOException {
+        JsonReader reader = new JsonReader(new InputStreamReader(in, "UTF-8"));
+        try {
+
+            // first array records first object that begins with certain letter
+            reader.beginObject();
+            reader.skipValue();
+
+            // second array contains actual data
+            // format: {name: string, fullname: string, expiry: array}
+            reader.beginArray();
+            while (reader.hasNext()) {
+                reader.beginObject();
+
+                while (reader.hasNext()) {
+                    String foodName = "";
+
+                    String name = reader.nextName();
+
+                    if (name.equals("name")) {
+                        String rawName = reader.nextString();
+                        foodName = rawName.substring(0, 1).toUpperCase() + rawName.substring(1);
+                    }
+                    else if (name.equals("expiry")) {
+                        readExpiry(db, reader, foodName);
+                    }
+                    else {
+                        reader.skipValue();
+                    }
+                }
+                reader.endObject();
+            }
+            reader.endArray();
+            reader.endObject();
+        }
+        finally {
+            reader.close();
+        }
+    }
+
+    private void readExpiry(SQLiteDatabase db, JsonReader reader, String foodName) throws IOException {
+
+        // TODO: add expTime to inventory table -> insert shelf life
+        long shelfLife = 0;
+        reader.beginArray();
+
+        while (reader.hasNext()) {
+            String invName = foodName;
+
+            // format: {type: string, refrigerator: days, pantry: days, freezer: days}
+            reader.beginObject();
+            while (reader.hasNext()) {
+                String expName = reader.nextName();
+                if (expName.equals("type")) {
+                    String type = reader.nextString();
+
+                    if (type.equals("refContainer")) {
+                        invName = foodName + " (container) ";
+                    }
+                    else if (!type.equals("default") && !type.equals("frozen")) {
+                        invName = foodName + " (" + type + ") ";
+                    }
+                }
+                else {
+                    long expDays = reader.nextLong();
+                    shelfLife = expDays * DateUtils.DAY_IN_MILLIS;
+                    if (expDays > 0) {
+                        if (expName.equals("refrigerator")) {
+                            invName = foodName + " (refrigerator) ";
+                        }
+                        else if (expName.equals("pantry")) {
+                            invName = foodName + " (pantry) ";
+                        }
+                        else if (expName.equals("freezer")) {
+                            invName = foodName +" (freezer) ";
+                        }
+
+                        // insert one item per type/storage combination (if exists)
+                        ContentValues values = new ContentValues();
+                        values.put(InventoryItem.COLUMN_NAME, invName);
+
+                        /*
+                         * Duplicates possible d/t imperfect scraping and time ranges
+                         * e.g. {type: default, refrigerator: -1, pantry: 30, freezer: 60},
+                         *      {type: default, refrigerator: 10, pantry: 60, freezer: -1}
+                         *
+                         * so just insert latest
+                         */
+                        db.insertWithOnConflict(InventoryItem.TABLE, null, values,
+                                SQLiteDatabase.CONFLICT_REPLACE);
+
+                    }
+                }
+
+            }
+            reader.endObject();
+        }
+        reader.endArray();
     }
 
     @Override
